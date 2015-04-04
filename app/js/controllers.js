9205ed91c76b7f7d3982f5fac161d2cfc89db8ea
Olexiy665 <alexey6mk@gmail.com>
2015-04-04 15:02:23 +0300

    [front-end] Angular+RequireJS done

--- app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by alexey on 16.03.15.
- */
-var placesControllers = angular.module('placesControllers', []);
-
-placesControllers.controller('PlaceListCtrl', ['$scope', 'Place',
-   function($scope, Place){
-       $scope.places = Place.query();
-   }]);
